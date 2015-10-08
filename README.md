edf02a0eba4cf6bedac60928fc49444afbd37ba8
Vladimir Lettiev <thecrux@gmail.com>
2015-10-08 12:26:21 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::VW - It's new $module
+
+# SYNOPSIS
+
+    use Test::VW;
+
+# DESCRIPTION
+
+Test::VW is ...
+
+# LICENSE
+
+Copyright (C) Vladimir Lettiev.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Vladimir Lettiev &lt;thecrux@gmail.com>
---
+++ lib/Test/VW.pm
@@ -0,0 +1,39 @@
+package Test::VW;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::VW - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::VW;
+
+=head1 DESCRIPTION
+
+Test::VW is ...
+
+=head1 LICENSE
+
+Copyright (C) Vladimir Lettiev.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Vladimir Lettiev E<lt>thecrux@gmail.comE<gt>
+
+=cut
+
